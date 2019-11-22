ce9e6689e02223bc6bbf16d2ce437aa800bf2c7e
jidongfang123 <17600570699@163.com>
2019-11-22 14:18:04 +0800

    新增接口

---
+++ src/main/java/com/tencent/client/service/WithdrawalService.java
@@ -0,0 +1,13 @@
+package com.tencent.client.service;
+
+import com.tencent.client.util.ResponseVo;
+
+public interface WithdrawalService {
+
+  ResponseVo withdrawalCaseCount();
+
+  ResponseVo executionNode();
+
+  ResponseVo distributionPartie();
+
+}
