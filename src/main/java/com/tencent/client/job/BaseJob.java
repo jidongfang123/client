7512f06b2f29780d8ba1d61e95c5d779f9086c8f
jidongfang-tc <17600570699@163.com>
2022-02-23 20:27:27 +0800

    删除无用代码

--- src/main/java/com/tencent/client/job/BaseJob.java
+++
@@ -1,9 +0,0 @@
-package com.tencent.client.job;
-
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-
-public interface BaseJob extends Job{
-    public void execute(JobExecutionContext context) throws JobExecutionException;
-}
